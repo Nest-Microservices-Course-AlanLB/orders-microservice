17ecb3d8fa73fc8ca4e99eb619a86469b68f5302
alanlb195 <alanlopezbarrientos@live.com.mx>
2025-04-21 23:24:50 -0500

    Initial commit - orders microservice connected to client-gateway and orders crud created

---
+++ src/common/dtos/order-pagination.dto.ts
@@ -0,0 +1,15 @@
+import { IsEnum, IsOptional } from "class-validator";
+import { PaginationDto } from "./pagination.dto";
+import { OrderStatusList } from "src/orders/enum/order.enum";
+import { OrderStatus } from "@prisma/client";
+
+
+
+export class OrderPaginationDto extends PaginationDto {
+
+    @IsOptional()
+    @IsEnum(OrderStatusList, {
+        message: `Valid status are ${ OrderStatusList }`
+    })
+    status: OrderStatus
+}
---
+++ src/common/index.ts
@@ -0,0 +1,3 @@
+export * from './dtos/order-pagination.dto';
+export * from './dtos/pagination.dto';
+export * from './exceptions/rpc-custom-exception.filter';
